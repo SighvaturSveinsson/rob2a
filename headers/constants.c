9ae0ca71730eedbcf503f7d54645c9128a4edca5
SighvaturSveinsson <sighvatursv373@nemi.tskoli.is>
2018-04-20 09:09:15 +0000

    uppfærði constants

--- headers/constants.c
+++ headers/constants.c
@@ -1,7 +1,7 @@
 const int potent_arm_limit = 1400;
 const int BASEDIST = 564;
 const int power = 65;
-const	int low_power = 15;
+const	int low_power = 45;
 const	int no_power = 0;
 const int line_threshold = 2500;
 const float BASETURN = 2.38;
